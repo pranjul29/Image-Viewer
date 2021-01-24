52722ae61170676140d2d64369f5eb1ec5fc18d0
pranjul29 <paras.paharia29@gmail.com>
2021-01-24 16:03:49 +0530

    Home Page Functionality Implemented

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,24 +1,136 @@
 import React, { Component } from 'react';
 import Login from './login/Login';
 import Home from './home/Home';
-import Profile from './profile/Profile';
+//import Profile from './profile/Profile';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Controller extends Component {
     constructor() {
         super();
         this.state = {
-            accessToken: "IGQVJWajJFZA0ZAJQ0xXZADVjRmp2cnA5UElxTkdKY2I2d21yWWtUdGFoNkRiaGk4ckZApS0UxdEkxeHBFeklrMkZAhTjA4ZAVhhYlJaVUVFSjlBTjBpYXhzM0pQZAGs1ZAmVxQzFyUmduYUtBMXlwd1dkbWhfSAZDZD",
+            baseUrl: "https://graph.instagram.com/me/media?fields=id,caption&access_token=",
+            postUrl: {
+                url1: "https://graph.instagram.com/",
+                url2: "?fields=id,media_type,media_url,username,timestamp&access_token="
+            },
+            /* Please do not modify the access token */
+            accessToken: "IGQVJWYTEzLU1NUGFuLXhIcmI3aEJFVXBxcTQ1cEJWOW43QTdFbVppcnpGVWZAISDhDaGh5TU02UFpxWTNCQlowajJDNWd0azVMMnFDME0xbUNNakIzYTA4NWdQdVh5QWRBckRVLVc0ak5qV3dhUnBoU3VZAM0pfYVp0eHEw",
+            username: "",
+            usernameSet: false,
+            posts: [],
+            filteredPosts: [],
+            showFilteredPosts: false,
+            tempList: [],
+            likeList: [...Array(8)].map(() => Math.floor(Math.random() * 20)),
+            likeDetails: new Array(8).fill(false),
+            follows: Math.floor(Math.random() * 20),
+            followedBy: Math.floor(Math.random() * 20),
+            commentsList: {
+                input1: ["Taken during Christmas", "2 years ago"],
+                input2: ["Such an old picture", "Lens flares?"],
+                input3: ["Sweetest thing on the planet!", "Ruby"],
+                input4: ["Taken 1 year ago", "Also taken during Christmas time"],
+                input5: ["Rocky", "This fellow is a survivor"],
+                input6: ["Yes Christmas time again", "and I like taking pictures of lights"],
+                input7: ["Credits to Matty for the picture", "Taken at the Yelagiri Peak"],
+                input8: ["Taken on my birthday", "Last year"]
+            },
+            tagsList: {
+                input1: ["#Christmas", "#Candles"],
+                input2: ["#Lights", "#LensFlares"],
+                input3: ["#Lab", "#Ruby"],
+                input4: ["#Christmas", "#Candles"],
+                input5: ["#Rocky", "#Survivor"],
+                input6: ["#Christmas", "#Lights"],
+                input7: ["#Trek", "#Yelagiri"],
+                input8: ["#Birthday", "#2019"]
+            }
         }
     }
 
+    componentDidMount() {
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({ posts: JSON.parse(this.responseText).data });
+            }
+        });
+        xhr.open("GET", this.state.baseUrl + this.state.accessToken);
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.send(data);
+    }
+
+    getPostDetailsById = (id) => {
+        let postData = null;
+        let xhrPost = new XMLHttpRequest();
+        let tempPostDetails = this.state.tempList
+        xhrPost.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                tempPostDetails.push(JSON.parse(this.responseText))
+            }
+        })
+        xhrPost.open("GET", this.state.postUrl.url1 + id + this.state.postUrl.url2 + this.state.accessToken);
+        xhrPost.setRequestHeader("Cache-Control", "no-cache");
+        xhrPost.send(postData);
+        return tempPostDetails
+    }
+
+    filterCaptions = (str) => {
+        this.setState({ filteredPosts: this.state.posts })
+        str.trim().length > 0 ? this.setState({ showFilteredPosts: true }) : this.setState({ showFilteredPosts: false })
+        let temp = this.state.posts;
+        let filtered = temp.filter(post => {
+            return post.caption.toLowerCase().includes(str.trim().toLowerCase())
+        })
+        this.setState({ filteredPosts: filtered })
+    }
+
+    updatelikeDetails = (id) => {
+        let temp = this.state.likeDetails
+        temp[id] ? temp[id] = false : temp[id] = true
+        this.setState({ likeDetails: temp })
+        this.setState({ usernameSet: true })
+    }
+
+    setUsername = (name) => {
+        this.setState({ username: name })
+        this.setState({ usernameSet: true })
+    }
+
+    addComments = (num, comment) => {
+        let temp = this.state.commentsList
+        temp[Object.keys(temp)[num]].push(comment)
+        this.setState({ commentsList: temp })
+    }
 
     render() {
-      return (
+
+        let postDetails = []
+        this.state.posts.forEach(post => {
+            postDetails = this.getPostDetailsById(post.id)
+        })
+
+        return (
             <Router>
                 <div>
                     <Route exact path='/'>
                         <Login accessToken={this.state.accessToken} />
+                    </Route>
+                    <Route exact path={'/home'}>
+                        <Home loggedIn={this.state.loggedIn}
+                              tagsList={this.state.tagsList}
+                              commentsList={this.state.commentsList}
+                              addComments={this.addComments}
+                              likeDetails={this.state.likeDetails}
+                              updatelikeDetails={this.updatelikeDetails}
+                              showFilteredPosts={this.state.showFilteredPosts}
+                              filteredPosts={this.state.filteredPosts}
+                              posts={this.state.posts}
+                              filterCaptions={this.filterCaptions}
+                              postDetails={postDetails}
+                              likeList={this.state.likeList} />
                     </Route>
                 </div>
             </Router>
---
+++ src/screens/home/Home.js
@@ -0,0 +1,180 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Header from '../../common/header/Header';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import img from '../../assets/ProfilePic.JPG';
+import Avatar from '@material-ui/core/Avatar';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import Favorite from '@material-ui/icons/Favorite';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Redirect } from 'react-router-dom';
+
+class Home extends Component {
+    constructor() {
+        super();
+        const accessToken = sessionStorage.getItem("access-token")
+        let loggedIn = true
+        if(accessToken === null) {
+            loggedIn = false
+        }
+        this.state = {
+            liked: false,
+            comment: "",
+            comments: ["", "", "", "", "", "", "", ""],
+            loggedIn
+        }
+    }
+
+    likeClickhandler = (id) => {
+        this.props.updatelikeDetails(id)
+    }
+
+    commentChangeHandler = (pos, e) => {
+        let temp = this.state.comments
+        temp[pos] = e.target.value
+        this.setState({ comments: temp })
+    }
+
+    addComment = (pos) => {
+        if (this.state.comments[pos].trim() !== "") {
+            this.props.addComments(pos, this.state.comments[pos])
+        }
+        let temp = this.state.comments
+        temp[pos] = ""
+        this.setState({ comments: temp })
+    }
+
+    render() {
+
+        console.log(this.props.posts)
+        if (!this.state.loggedIn) {
+            return <Redirect to="/" />
+        }
+
+        let temp = 0
+
+        let tempusername = ""
+        let tempSrc
+        let tempTimeStamp
+        let tempDate
+        let tempMonth
+        let tempYear
+        let tempHour
+        let tempMin
+        let tempSec
+        let likeNumber
+        let counter = 0
+        let commentsValue = 0
+        let tagValue = 0
+        let commentsList = this.props.commentsList
+        let tagsList = this.props.tagsList
+
+        let displayPosts
+
+        this.props.showFilteredPosts ? displayPosts = this.props.filteredPosts : displayPosts = this.props.posts
+
+        return (
+            <div className="outerContainer">
+                <Header dispalySearchBar={true}
+                        displayUserProfileIcon={true}
+                        filterCaptions={this.props.filterCaptions}
+                />
+                <div className="home">
+                    <div className="homeMain">
+                        <div className="cardContainer">
+                            {
+                                displayPosts.map(post => {
+                                    likeNumber = this.props.likeList[counter]
+                                    counter++
+                                    this.props.postDetails.forEach(thispost => {
+                                        if (thispost.id === post.id) {
+                                            tempusername = thispost.username
+                                            tempSrc = thispost.media_url
+                                            tempTimeStamp = new Date(thispost.timestamp)
+                                            tempDate = tempTimeStamp.getDate()
+                                            tempMonth = tempTimeStamp.getMonth() + 1
+                                            tempYear = tempTimeStamp.getFullYear()
+                                            tempHour = tempTimeStamp.getHours()
+                                            tempMin = tempTimeStamp.getMinutes()
+                                            tempSec = tempTimeStamp.getSeconds()
+                                        }
+                                    });
+                                    return <Card key={post.id} id={post.id} className="cardStyle">
+                                        <div>
+                                            <CardHeader className="cardHeader" avatar={
+                                                <Avatar className="avatar" src={img} sizes="small" />
+                                            }
+                                                        title={tempusername}
+                                                        subheader={tempDate + "/" + tempMonth + "/" + tempYear + " " + tempHour + ":" + tempMin + ":" + tempSec} />
+                                        </div>
+                                        <div className="cardContent">
+                                            <CardContent>
+                                                <div className="imgSection">
+                                                    <img className="image" src={tempSrc} alt={post.caption} />
+                                                </div>
+                                                < hr />
+                                                <div className="postDetails">
+                                                    <div className="caption"><Typography variant="h5">{post.caption}</Typography></div>
+                                                    <div className="tags" {...tagValue++}>
+                                                        {
+                                                            this.props.tagsList[Object.keys(tagsList)[tagValue - 1]].map(tag => {
+                                                                temp++
+                                                                return <span key={"tag"+temp}>{tag}&nbsp;</span>
+                                                            })
+                                                        }
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    {
+                                                        this.props.likeDetails[counter - 1] ? <div className="likeSection"><Favorite id={2} style={{ color: "red" }} className="likeButton" onClick={this.likeClickhandler.bind(this, counter - 1)} /><span>{likeNumber + 1} likes</span></div> :
+                                                            <div className="likeSection"><FavoriteBorderIcon id={2} className="likeButton" onClick={this.likeClickhandler.bind(this, counter - 1)} /><span>{likeNumber} likes</span></div>
+                                                    }
+                                                </div>
+                                                <div className="commentSection">
+                                                    {
+                                                        <div {...commentsValue++}>
+                                                            <div id="comments" className="comments">
+                                                                <div className="addedCommentsSection">
+                                                                    {
+                                                                        this.props.commentsList[Object.keys(commentsList)[commentsValue - 1]].map(comment => {
+                                                                            temp++
+                                                                            return <div key={post.id + temp}>
+                                                                                <span className="bold">{tempusername}:</span>
+                                                                                <span>{comment}</span>
+                                                                            </div>
+                                                                        })
+                                                                    }
+                                                                </div>
+                                                            </div>
+                                                            <div className="addComment">
+                                                                <FormControl className="commentInput" >
+                                                                    <InputLabel htmlFor={"input" + commentsValue}>Add a comment</InputLabel>
+                                                                    <Input id={"input" + commentsValue} type="text" value={this.state.comments[commentsValue - 1]} onChange={this.commentChangeHandler.bind(this, commentsValue - 1)} />
+                                                                </FormControl>
+                                                                <Button className="addButton" variant="contained" color="primary" onClick={this.addComment.bind(this, commentsValue - 1)}>
+                                                                    ADD
+                                                                </Button>
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                </div>
+                                            </CardContent>
+                                        </div>
+                                    </Card>
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
